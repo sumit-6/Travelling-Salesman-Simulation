6bf157a27f61e08281c7536987be8967f0dec542
sumit-6 <75926522+sumit-6@users.noreply.github.com>
2022-06-25 20:17:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ app = Flask(__name__)
 
 Brute_Cities = ''
 Dynamic_Cities=''
+Genetic_Cities=''
 
 @app.route('/', methods = ['POST', 'GET'])
-def index2():
+def index():
     return render_template('index.html')
 @app.route('/brute', methods = ['POST', 'GET'])
-def index1():
+def brute():
     global Brute_Cities
     if request.method == 'POST':
         task_content = request.form['playto']
@@ -19,7 +20,7 @@ def index1():
         return render_template('index.html')
 
 @app.route('/dynamic', methods = ['POST', 'GET'])
-def index():
+def dynamic():
     global Dynamic_Cities
     if request.method == 'POST':
         task_content = request.form['playto']
@@ -28,8 +29,18 @@ def index():
     else:
         return render_template('index.html')
 
+@app.route('/genetic', methods = ['POST', 'GET'])
+def genetic():
+    global Genetic_Cities
+    if request.method == 'POST':
+        task_content = request.form['playto']
+        Genetic_Cities = task_content
+        return  render_template('GetPoints(genetic).html',task_content = task_content)
+    else:
+        return render_template('index.html')
+
 @app.route('/playBrute', methods = ['POST', 'GET'])
-def getPoints():
+def getPointsBrute():
     if request.method == 'POST':
         task_content = '0 ' + Brute_Cities + ' '
         for i in range((int(Brute_Cities))*2):
@@ -42,7 +53,7 @@ def getPoints():
         return render_template('index.html')
 
 @app.route('/playRandomBrute', methods = ['POST', 'GET'])
-def getPoints1():
+def getPointsRandomBrute():
     if request.method == 'POST':
         task_content = '1 ' + Brute_Cities
         return render_template('Brute.html', task_content = task_content)
@@ -50,7 +61,7 @@ def getPoints1():
         return render_template('index.html')
 
 @app.route('/playDynamic', methods = ['POST', 'GET'])
-def getPoints2():
+def getPointsDynamic():
     if request.method == 'POST':
         task_content = '0 ' + Dynamic_Cities + ' '
         for i in range((int(Brute_Cities))*2):
@@ -63,12 +74,38 @@ def getPoints2():
         return render_template('index.html')
 
 @app.route('/playRandomDynamic', methods = ['POST', 'GET'])
-def getPoints12():
+def getPointsRandomDynamic():
     if request.method == 'POST':
         task_content = '1 ' + Dynamic_Cities
         return render_template('Dynamic.html', task_content = task_content)
     else:
         return render_template('index.html')
+
+@app.route('/playGenetic', methods = ['POST', 'GET'])
+def getPointsGenetic():
+    if request.method == 'POST':
+        task_content = '0 ' + Genetic_Cities + ' '
+        for i in range((int(Genetic_Cities))*2):
+            if(i < (int(Genetic_Cities))*2 - 1):
+                task_content += request.form[str(i)] + ' '
+            else:
+                task_content += request.form[str(i)]
+        return render_template('genetic.html', task_content = task_content)
+    else:
+        return render_template('index.html')
+
+@app.route('/playRandomGenetic', methods = ['POST', 'GET'])
+def getPointsRandomGenetic():
+    if request.method == 'POST':
+        task_content = '1 ' + Genetic_Cities
+        return render_template('genetic.html', task_content = task_content)
+    else:
+        return render_template('index.html')
+
+
+#@app.route('/genetic', methods = ['POST', 'GET'])
+#def genetic():
+#    return render_template('genetic.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
